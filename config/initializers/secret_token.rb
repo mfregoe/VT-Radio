3f46b957ab89fa7c6eeb94fa6604694cccbf3282
Gregory Grillone <greg.grillone@gmail.com>
2013-10-11 18:59:29 -0400

    NEW RAILS APP :)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackVT2013Teamzeusbear::Application.config.secret_token = '2ce6627ce99de583d39f07ccec827ecfdfdf30f40da65d1bce6aa89de6011da4c4e66a15e515cc1185bb882ae4c4ecc9b49ec28c92e7063c209b7b1227a39777'
