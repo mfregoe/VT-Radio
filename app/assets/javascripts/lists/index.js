dced4fe276179619ee165e514efb74c9645d2f89
Gregory Grillone <greg.grillone@gmail.com>
2013-10-12 12:16:16 -0400

    fix filter by radius defect

--- app/assets/javascripts/lists/index.js
+++ app/assets/javascripts/lists/index.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     // fetch json data
     $.getJSON(window.location.pathname, function(data) {
       if(data.length === 0) {
-        $('#data-container').html('No results found within your radius');
+        $('#data-container').html('<div style="text-align: center;">No results found within your radius</div>');
       }
       else {
         collection = data;
@@ -30,18 +30,21 @@ $(document).ready(function() {
 
     if(localStorage.getItem('latitude') !== null && localStorage.getItem('longitude') !== null) {
       origin = localStorage.getItem('latitude') + ',' + localStorage.getItem('longitude');
+      // google api call
+      $.getJSON('http://maps.googleapis.com/maps/api/distancematrix/json?origins=' + origin + '&destinations=' + destinations + '&mode=driving&sensor=false', function(data) {
+        convertMetersToMiles(data.rows[0].elements);
+      });
     }
     else if(localStorage.getItem('zip') !== null) {
       origin = localStorage.getItem('zip');
+      // google api call
+      $.getJSON('http://maps.googleapis.com/maps/api/distancematrix/json?origins=' + origin + '&destinations=' + destinations + '&mode=driving&sensor=false', function(data) {
+        convertMetersToMiles(data.rows[0].elements);
+      });
     }
     else {
-      origin = '05401';
+      renderCollection();
     }
-
-    // google api call
-    $.getJSON('http://maps.googleapis.com/maps/api/distancematrix/json?origins=' + origin + '&destinations=' + destinations + '&mode=driving&sensor=false', function(data) {
-      convertMetersToMiles(data.rows[0].elements);
-    });
   }
 
   function convertMetersToMiles(arr) {
@@ -81,13 +84,21 @@ $(document).ready(function() {
 
   // render the data collection in UI
   function renderCollection() {
+    var count = 0;
     _.each(collection, function(item) {
-      var html = '<article class="post-2178 post type-post status-publish format-standard hentry category-blog post clearfix"><figure class="post-image">';
-      html += item.frequency + '</figure><div class="post-content post-station"><h1 class="post-title">';
-      html += item.call_letters + ' - ' + item.name + '</h1><p class="post-meta">';
-      html += item.band + ' - ' + item.city + ', ' + item.state + '</p><p>';
-      html += 'Phone: ' + item.phone + '<br/>Genres: ' + item.genre + '</p></div><div class="post-social"><p class="post-meta">Share It:</p><span class="social-link-item twitter"></span><span class="social-link-item facebook"></span></div></article>';
-      $('#data-container').append(html);
+      if(item.show || item.show === undefined) {
+        count++;
+        var html = '<article class="post-2178 post type-post status-publish format-standard hentry category-blog post clearfix"><figure class="post-image">';
+        html += item.frequency + '</figure><div class="post-content post-station"><h1 class="post-title">';
+        html += item.call_letters + ' - ' + item.name + '</h1><p class="post-meta">';
+        html += item.band + ' - ' + item.city + ', ' + item.state + '</p><p>';
+        html += 'Phone: ' + item.phone + '<br/>Genres: ' + item.genre + '</p></div><div class="post-social"><p class="post-meta">Share It:</p><span class="social-link-item twitter"></span><span class="social-link-item facebook"></span></div></article>';
+        $('#data-container').append(html);
+      }
     });
+
+    if(count === 0) {
+      $('#data-container').html('<div style="text-align: center;">No results found within your radius</div>');
+    }
   }
 });
